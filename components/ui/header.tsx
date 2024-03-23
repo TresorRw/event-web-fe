43370de1b8a7f57f0d81c5fd18207f6cc86f3258
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-23 20:07:53 +0200

    UI components

---
+++ components/ui/header.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react'
+import Container from './container'
+import Link from 'next/link'
+import { Button } from './button'
+import { ModeToggle } from "@/components/theme-changer";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
+          <div className="flex items-center">
+            <Link href="/" className='ml-4 lg:ml-0'>
+              <h1 className="text-xl font-bold text-primary">TresEvents</h1>
+            </Link>
+          </div>
+          <div className="flex items-end space-x-4">
+            <ModeToggle />
+            <Button className='dark:text-white rounded-2xl'>Join us</Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
