25ea1ffe4300de933bde38650c004ce56b495121
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-27 16:16:02 +0200

    Added sign in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import dynamic from 'next/dynamic'
+const SignInForm = dynamic(() => import("@/components/ui/forms/signin"), { ssr: false })
+import { Separator } from "@/components/ui/separator"
+import Link from "next/link"
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className="bg-gray-50 p-4 flex justify-center dark:bg-inherit w-full">
+      <Card className="md:w-1/2 lg:w-1/3 w-full">
+        <CardHeader>
+          <CardTitle className="text-2xl">Welcome at TresEvents</CardTitle>
+          <CardDescription>Elevate Your Event Experience</CardDescription>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <SignInForm />
+        </CardContent>
+        <CardFooter>
+          <p>No account yet? <Link className="text-primary" href={"/signup"}>Create one here</Link> </p>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
