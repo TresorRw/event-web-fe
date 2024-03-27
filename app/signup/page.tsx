e196d738affee83c626ec41de86bf41b2fec0378
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-27 16:10:51 +0200

    Sign up page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,22 +6,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import dynamic from 'next/dynamic'
+const SignUpForm = dynamic(() => import("@/components/ui/forms/signup"), { ssr: false })
 import { Separator } from "@/components/ui/separator"
+import Link from "next/link"
 
 const SignUp = () => {
   return (
     <div className="bg-gray-50 p-4 flex justify-center dark:bg-inherit w-full">
-      <Card className="md:w-1/2 lg:w-1/3 w-full mt-32">
+      <Card className="md:w-1/2 lg:w-1/3 w-full">
         <CardHeader>
           <CardTitle className="text-2xl">Create your account at TresEvents</CardTitle>
           <CardDescription>Elevate Your Event Experience</CardDescription>
           <Separator />
         </CardHeader>
         <CardContent>
-          {"SIGN UP FORM WILL BE PLACED HERE"}
+          <SignUpForm />
         </CardContent>
         <CardFooter>
-          {"OTHER CONTENT GOES HERE"}
+          <p>Already our member? <Link className="text-primary" href={"/signin"}>Login here</Link> </p>
         </CardFooter>
       </Card>
 
