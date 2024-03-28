bca4080410a47ace7b4fd466c6a7c1ff447c1c28
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-28 11:32:50 +0200

    Added <Toaster /> wrapper on the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 import Header from "@/components/ui/header";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
