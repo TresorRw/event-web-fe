82cdc931d92fc450aa976dd12c49677e1f191534
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 17:36:47 +0200

    added event categories enum

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,13 @@
 export const BackendAPI = process.env.NEXT_PUBLIC_API_URL || process.env.API_URL;
+
+export enum EventCategories {
+  CONFERENCE = "conference",
+  TRAVEL_ADVENTURES = "travel_adventures",
+  CHARITY = "charity",
+  SPORTS_FITNESS = "sports_fitness",
+  CINEMA_MEDIA = "cinema_media",
+  ARTS_CULTURE = "arts_culture",
+  CONCERT = "concert",
+  FOOD_DRINKS = "food_drinks",
+  PERFORMANCE = "performance",
+}
