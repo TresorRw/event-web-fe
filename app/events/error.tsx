37ccdecc1fb5c2cce26abbaad954ddefae15db46
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 19:14:39 +0200

    added alert message

---
+++ app/events/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <Button
+        onClick={
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,13 +1,39 @@
 import Container from "@/components/ui/container"
+import EventCard from "@/components/ui/event-card";
+import { IGetGoodEventResponse } from "@/interfaces";
+import { BackendAPI } from "@/lib/constants"
+import axios from "axios"
 
-const EventsPage = () => {
+const getAllEvents = async () => {
+  const events = await axios.get(`${BackendAPI}/api/events`);
+  return events.data as IGetGoodEventResponse;
+}
+
+const EventsPage = async () => {
+  const events = await getAllEvents();
+
   return (
     <Container>
-      <div className="w-full pt-4">
-        <h1 className="text-3xl font-bold">Events</h1>
+      <div className="w-full pt-4 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {events.data.map((event) => (
+            <EventCard
+              endDateTime={event.endDateTime}
+              key={event._id}
+              _id={event._id}
+              price={event.price}
+              startDateTime={event.startDateTime}
+              name={event.name}
+              description={event.description}
+              image=""
+              organizer={event.organizer}
+              location={event.location}
+              category={event.category} />
+          ))}
+        </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
 export default EventsPage
