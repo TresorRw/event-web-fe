d87752e44806329d990979378645d8d1028af5d4
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 19:17:18 +0200

    Error page to be returned in case there is any

--- app/events/error.tsx
+++ app/events/error.tsx
@@ -11,18 +11,21 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    console.error(error)
+    // console.error(error)
   }, [error])
+
+  const handlePageRefresh = () => {
+    reset()
+    location.reload()
+  }
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Something went wrong! Our team is working to return things to normal way of operating</h2>
       <Button
-        onClick={
-          () => reset()
-        }
+        onClick={handlePageRefresh}
       >
-        Try again
+        Reload the page
       </Button>
     </div>
   )
