627d32e7e8b47ca191e3b99260da09177a1826a5
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 17:30:45 +0200

    Event interfaces

---
+++ interfaces/events.ts
@@ -0,0 +1,31 @@
+import { EventCategories } from "@/lib/constants"
+
+export interface IEvent {
+  _id: string,
+  name: string,
+  category: EventCategories,
+  description: string,
+  startDateTime: Date,
+  endDateTime: Date,
+  location: string,
+  price: number,
+  image?: string,
+  organizer: IEventOrganizer,
+}
+
+export interface IEventOrganizer {
+  _id: string,
+  email: string,
+  displayName: string,
+}
+
+export interface IGetGoodEventResponse {
+  statusCode: number,
+  message: string,
+  data: IEvent[],
+}
+
+export interface IErrorResponse {
+  statusCode: number,
+  message: string
+} 
