7d1cfbd3685285103e997f66043ad33762312c49
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 17:35:36 +0200

    Added proper domains for external images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['tecdn.b-cdn.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tecdn.b-cdn.net',
+        port: '',
+        pathname: '/img/new/standard/nature/184.jpg',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
