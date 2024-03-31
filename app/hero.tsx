f713fe2cf437daaff155e2ef514305f013b57ad4
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-31 17:37:37 +0200

    Removed title on hero section

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,14 +1,11 @@
 import { Button } from '@/components/ui/button'
-import { Separator } from "@/components/ui/separator"
 import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
   return (
     <div className="bg-gray-50 dark:bg-inherit w-full">
-      <div className="container mx-auto  w-full lg:w-1/2 text-center py-12">
-        <h1 className="text-3xl text-primary font-bold mb-4">TresEvents</h1>
-        <Separator className='w-1/2 mx-auto mb-10' />
+      <div className="container mx-auto w-full lg:w-1/2 text-center py-12 mt-20">
         <h2 className="text-2xl font-bold mb-4">Empower Your Events: Seamlessly Plan, Manage, and Experience Every Moment</h2>
         <p className="text-lg mt-10">Welcome to Your Ultimate Event Hub: Where Organizers Craft Unforgettable Experiences and Attendees Discover Thrilling Moments, All in One Dynamic Platform!</p>
 
