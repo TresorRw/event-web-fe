c3f5dccab58df3c185c766dcf8ae713867b394de
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-04 17:10:13 +0200

    Added index.ts as default exporter for all functions

---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export * from "./Error"
+export * from "./axios-client"
+export * from "./constants"
+export * from "./utils"
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertDate = (date: Date) => {
+  return new Date(date).toDateString() + ", " + new Date(date).toLocaleTimeString();
+}
