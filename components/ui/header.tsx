f09f056dd10161fae0fb185a807d74c99dec3ba2
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-08 22:01:00 +0200

    Refactor: formatting Header component for clear reading

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,56 +1,68 @@
-'use client'
+"use client";
 
-import Container from './container'
-import Link from 'next/link'
+import Container from "./container";
+import Link from "next/link";
 import { ModeToggle } from "@/components/theme-changer";
 import classNames from "classnames";
-import { usePathname } from 'next/navigation';
-import { Button } from "@/components/ui/button"
-import { useAuthStore } from '@/app/store/auth.store';
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/app/store/auth.store";
 
 const Header = () => {
   const pathName = usePathname();
-  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   return (
     <header className="border-b">
       <Container>
         <div className="relative sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
           <div className="flex items-center">
-            <Link href="/" className='ml-4 lg:ml-0'>
+            <Link href="/" className="ml-4 lg:ml-0">
               <h1 className="text-xl font-bold text-primary">TresEvents</h1>
             </Link>
           </div>
           <div className="flex items-center justify-between space-x-2 md:space-x-4">
-            <Link className={classNames({
-              'text-primary': pathName === '/events',
-            })} href={"/events"}>Events</Link>
-            <Link className={classNames({
-              'text-primary': pathName === '/discover',
-            })} href={"/discover"}>Discover</Link>
             <Link
               className={classNames({
-                'text-primary': pathName === '/contact',
-              })} href={"/contact"}>Contact</Link>
+                "text-primary": pathName === "/events",
+              })}
+              href={"/events"}
+            >
+              Events
+            </Link>
+            <Link
+              className={classNames({
+                "text-primary": pathName === "/discover",
+              })}
+              href={"/discover"}
+            >
+              Discover
+            </Link>
+            <Link
+              className={classNames({
+                "text-primary": pathName === "/contact",
+              })}
+              href={"/contact"}
+            >
+              Contact
+            </Link>
           </div>
           <div className="flex items-center justify-center space-x-4">
             <ModeToggle />
-            {isAuthenticated ?
-              <Link
-                className={classNames({
-                  'text-primary': pathName === '/in/profile',
-                })} href={"/in/profile"}>
-                <Button className='rounded-xl dark:text-white'>Profile</Button>
-              </Link> :
-              <Link href={"/signin"}>
-                <Button className='rounded-xl dark:text-white'>Login</Button>
+            {isAuthenticated ? (
+              <Link href="/in/profile">
+                <Button className="rounded-xl dark:text-white">Profile</Button>
               </Link>
-            }
+            ) : (
+              <Link href={"/signin"}>
+                <Button className="rounded-xl dark:text-white">Login</Button>
+              </Link>
+            )}
           </div>
         </div>
       </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
