7de77669f909ce280e15a11d980c0ec926227044
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-08 20:08:52 +0200

    Added persistent storage

--- app/store/auth.store.ts
+++ app/store/auth.store.ts
@@ -1,37 +1,46 @@
-"use client"
-import { ILoggedUser } from "@/interfaces"
-import {create} from "zustand"
+"use client";
+import { ILoggedUser } from "@/interfaces";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export type AuthStore = {
-  isAuthenticated: boolean,
-  authToken: string,
-  loggedInUser: ILoggedUser,
-  saveUserData: (authToken: string, loggedInUser: ILoggedUser) => void,
-  clearUserData: () => void,
-}
+  isAuthenticated: boolean;
+  authToken: string;
+  loggedInUser: ILoggedUser;
+  saveUserData: (authToken: string, loggedInUser: ILoggedUser) => void;
+  clearUserData: () => void;
+};
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  isAuthenticated: false,
-  authToken: "",
-  loggedInUser: {
-    name: "",
-    role: null
-  },
-  clearUserData: () => {
-    set({
+export const useAuthStore = create<AuthStore>(
+  persist(
+    (set) => ({
       isAuthenticated: false,
       authToken: "",
       loggedInUser: {
         name: "",
-        role: null
-      }
-    })
-  },
-  saveUserData: (authToken, loggedInUser) => {
-    set({
-        isAuthenticated: true,
-        authToken,
-        loggedInUser,
-      })
-  },
-}));
+        role: null,
+      },
+      clearUserData: () => {
+        set({
+          isAuthenticated: false,
+          authToken: "",
+          loggedInUser: {
+            name: "",
+            role: null,
+          },
+        });
+      },
+      saveUserData: (authToken, loggedInUser) => {
+        set({
+          isAuthenticated: true,
+          authToken,
+          loggedInUser,
+        });
+      },
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => sessionStorage)
+    }
+  ),
+);
