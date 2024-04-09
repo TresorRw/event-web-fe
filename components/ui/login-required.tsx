4f2628d765345a0328675350b35bfd022457929d
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-09 22:01:36 +0200

    Login required component

---
+++ components/ui/login-required.tsx
@@ -0,0 +1,16 @@
+import { Alert, AlertDescription } from "@/components/ui/alert"
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
+
+const LoginRequired = () => {
+  
+  return (
+    <Alert variant="destructive">
+      <ExclamationTriangleIcon className="h-4 w-4" />
+      <AlertDescription>
+        You need to be logged in to book a ticket. Please sign in or register.
+      </AlertDescription>
+    </Alert>
+  )
+}
+
+export default LoginRequired
