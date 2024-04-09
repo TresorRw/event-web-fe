6f5037965c4150b8ffda72224d504d7c9c1b6461
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-09 17:53:20 +0200

    Added speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Header from "@/components/ui/header";
 import { Toaster } from "@/components/ui/toaster"
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
