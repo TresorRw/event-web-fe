8204d8056587ab1d93e7661108e12d8140ee204e
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-10 19:17:06 +0200

    Adde a loading gif to show while waiting to render requested page

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/ui/container'
+import Image from 'next/image'
+
+const LoadingUI = () => {
+  return (
+    <Container>
+      <div className='w-full p-4 h-96 flex justify-center items-center'>
+        <Image src={"/loading.gif"} width={100} height={100} alt='Loading GIF' />
+      </div>
+    </Container>
+  )
+}
+
+export default LoadingUI
