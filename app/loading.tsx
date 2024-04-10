ce9dbfb221cb508b31a89954f39b40206832ca2c
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-10 23:03:57 +0200

    Added unoptimized property on loading gif

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,7 +5,7 @@ const LoadingUI = () => {
   return (
     <Container>
       <div className='w-full p-4 h-96 flex justify-center items-center'>
-        <Image src={"/loading.gif"} width={100} height={100} alt='Loading GIF' />
+        <Image unoptimized src={"/loading.gif"} width={100} height={100} alt='Loading GIF' />
       </div>
     </Container>
   )
