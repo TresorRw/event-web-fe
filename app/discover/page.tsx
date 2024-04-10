a86c79ccbce1b92e2ec2b940c7f0788351d4ce28
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-10 18:32:15 +0200

    Discover events page with search and filters

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,6 +1,15 @@
+import Container from "@/components/ui/container"
+import SearchContent from "@/components/ui/forms/discover"
+
 const DiscoverPage = () => {
   return (
-    <div>DiscoverPage</div>
+    <Container>
+      <div className="w-full pt-4 px-2 flex flex-col items-center lg:p-4 text-center">
+        <h1 className="text-2xl font-semibold mt-5">Discover Your Next Event</h1>
+        <h5 className="text-xs mb-5">Search, Explore, and Attend Exciting Gatherings Near You!</h5>
+        <SearchContent />
+      </div>
+    </Container>
   )
 }
 
