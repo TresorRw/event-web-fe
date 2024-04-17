ce6316e8e9bc86d604eb9953a09e9a1917188e59
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-17 23:54:35 +0200

    Added new page for profile

---
+++ app/profile/page.tsx
@@ -0,0 +1,18 @@
+import Container from "@/components/ui/container";
+
+export const metadata = {
+  title: "My account",
+  description: "Manage your account",
+};
+
+const ProfilePage = () => {
+  return (
+    <Container>
+      <div>
+        <h1>Profile Page</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default ProfilePage;
