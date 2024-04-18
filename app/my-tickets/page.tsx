23a2563f17945f5d8a76c6530de10fef44ce0d03
TresorRw <alaintresorcyusa683@gmail.com>
2024-04-18 11:43:13 +0200

    Added breadcumb on my-tickets pae

--- app/my-tickets/page.tsx
+++ app/my-tickets/page.tsx
@@ -1,13 +1,46 @@
+import { SlashIcon } from "@radix-ui/react-icons";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import Container from "@/components/ui/container";
 import Tickets from "./tickets";
 import { Suspense } from "react";
 import LoadingUI from "../loading";
+import Link from "next/link";
 
 const AttendeeTicketsPage = () => {
   return (
     <Container>
       <div className="w-full pt-4 px-2">
-        <h1>Tickets Page</h1>
+        <Breadcrumb className="mb-5">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/">Home</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/events">Events</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon />
+            </BreadcrumbSeparator>
+            <BreadcrumbItem>
+              <BreadcrumbPage>My Tickets</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
         <Suspense fallback={<LoadingUI />}>
           <Tickets />
         </Suspense>
